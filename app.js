03d30f97dd81dd6d95371ebc97064ae3afd4fd45
Pedro Fidalgo <pedrofidalgo7@gmail.com>
2018-12-05 08:49:54 +0000

    Section 6 Lecture 79 done Added DOMStrrings object do the UI controller, we will use this to add our UI classes Returned this object to make it available to the APP controller Returned an object with the UI values On the App Controller, added event listen so when we press either the btn or enter key We will get an object with all the input values when we use those eventlisteners

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
-	var x = 23;
-	var add = function(a) {
-		return x + a;
-	}
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
+
+
+
 })();
 
+
+//UI CONTROLLER
 var UIController = (function() {
-	//Some code later
+
+	var DOMStrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	}
+
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector(DOMStrings.inputType).value, // will be either 'inc' or 'exp'
+				description: document.querySelector(DOMStrings.inputDescription).value,
+				value: document.querySelector(DOMStrings.inputValue).value
+			};
+		},
+		getDOMStrings: function() {
+			return DOMStrings;
+		}
+	};
 
 })();
 
+
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-	var z = budgetCtrl.publicTest(10);
+	var DOM = UICtrl.getDOMStrings();
 
-	return {
-		anotherPublic: function() {
-			console.log(z);
-		}
+	var ctrlAddItem = function() {
+
+		// 1. get the field input data
+		var input = UICtrl.getInput();
+		console.log(input);
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate de budget
+
+		// 5. Display the budget on the UI
 	}
+
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+			event.preventDefault();
+		}
+	});
+
 
 })(budgetController, UIController);
